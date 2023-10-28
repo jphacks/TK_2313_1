6bcb4f713722e4481bb625dccc0d5d40a99184fc
miuye256 <fallioss13@outlook.jp>
2023-10-29 03:30:49 +0900

    takanawa_chang実装

---
+++ Assets/Script/characterController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    Animator animator;
+    int sitNum = 0;
+
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetInteger("State", -1);
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            animator.SetInteger("State", 0);    // ë“ã@
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetInteger("State", 1);    // éËÇêUÇÈ
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetInteger("State", 2);    // óxÇÈ
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("SitTrigger");  // ç¿ÇÈ
+        }
+    }
+}
