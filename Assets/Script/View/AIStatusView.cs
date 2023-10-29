6c8487a259c2bd1ab689309ef2c5a16d91c0a306
MogamiTsuchikawa <30793866+MogamiTsuchikawa@users.noreply.github.com>
2023-10-29 10:30:31 +0900

    Create AIStatusView.cs

---
+++ Assets/Script/View/AIStatusView.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace View
+{
+    public class AIStatusView
+    {
+        [SerializedField] MeshRenderer _headObjectRenderer;
+        public enum Status
+        {
+            Normal = 0, Listening, Thinking, Error
+        }
+        private Color[] _statusColors = new[]{Color.cyan, Color.white, Color.yellow, Color.red};
+
+        public Status CurrentStatus
+        {
+            set
+            {
+                _headObjectRenderer.material.color = _statusColors[(int)value];
+            }
+        }
+    }
+}
+        
